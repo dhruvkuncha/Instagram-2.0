f5e821fb3fcfc95b01fefed62204605b4a4b6659
Dhruv Kuncha <kunchadhruv03@outlook.com>
2022-06-26 18:17:53 +0200

    fourth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,7 +9,7 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret:process.env.SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   pages: {
     signIn: "/auth/signin",
   },
